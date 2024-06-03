fa77d4795ef78cc3824cfbb3c786831c3b8d8027
Nguyen Le Gia Huy <job.nlgiahuy@gmail.com>
2024-06-03 18:18:07 +0700

    first time

---
+++ src/test/java/com/group_eight/website_quan_li_hoat_dong_tu_thien/WebsiteQuanLiHoatDongTuThienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_eight.website_quan_li_hoat_dong_tu_thien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteQuanLiHoatDongTuThienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
